d391afe97bbf8c50414eed01b979f67e27836fa4
johnsonmercyi <johnsonmercy.ng@gmail.com>
2023-11-12 04:35:44 +0100

    Just a few tweak

--- src/main/java/com/javadb/repository/AccountTypeRepository.java
+++ src/main/java/com/javadb/repository/AccountTypeRepository.java
@@ -10,102 +10,99 @@ import java.util.UUID;
 import com.javadb.bean.AccountType;
 import com.javadb.db.Database;
 
-public class AccountTypeRepository extends Database implements Repository <AccountType, UUID> {
+public class AccountTypeRepository extends Database implements Repository<AccountType, UUID> {
 
-    private ResultSet resultSet;
-    
-    @Override
-    public Optional<AccountType> add(AccountType accountType) {
-        String sql = "";
-        int inserted = -1;
-        AccountType storedAccount = null;
+  private ResultSet resultSet;
 
-        if (findBy(accountType.getId()).isPresent()) {
-            sql = "UPDATE account_type SET type=?";
-            inserted = postQuery(sql, accountType.getType());
-            storedAccount = findBy(accountType.getId()).get();
-        } else {
-            sql = "INSERT INTO account_type (id, typ) VALUES (?, ?)";
-            inserted = postQuery(sql, accountType.getId(), accountType.getType());
-        
-            storedAccount = findBy(accountType.getId()).get();
-          
-       
-        
-        }
+  @Override
+  public Optional<AccountType> add(AccountType accountType) {
+    String sql = "";
+    int inserted = -1;
+    AccountType storedAccount = null;
 
-        if (inserted != -1) {
-        return Optional.ofNullable(storedAccount);
-        }
-        
-        return Optional.empty();
+    if (findBy(accountType.getId()).isPresent()) {
+      sql = "UPDATE account_type SET type=?";
+      inserted = postQuery(sql, accountType.getType());
+      storedAccount = findBy(accountType.getId()).get();
+    } else {
+      sql = "INSERT INTO account_type (id, typ) VALUES (?, ?)";
+      inserted = postQuery(sql, accountType.getId(), accountType.getType());
+
+      storedAccount = findBy(accountType.getId()).get();
     }
 
-    @Override
-    public List<AccountType> getAll() {
-        List<AccountType> list = new ArrayList<>();
-
-        try {
-        String sql = "SELECT * FROM account_type;";
-        resultSet = getQuery(sql);
-        while (resultSet.next()) {
-            AccountType accountType = new AccountType();
-
-            accountType.setId(UUID.fromString(resultSet.getObject(1).toString()));
-            accountType.setType(resultSet.getString(2));
-            accountType.setCreated(resultSet.getDate(3));
-            accountType.setUpdated(resultSet.getDate(4));
-
-            list.add(accountType);
-        }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        closeConnection();
-        return list;
+    if (inserted != -1) {
+      return Optional.ofNullable(storedAccount);
     }
 
-    @Override
-    public Optional<AccountType> findBy(UUID id) {
-        try {
-            String sql = "SELECT * FROM account_type WHERE account_type.id = ?";
-            return this.fetch(sql, id);
-          } catch (Exception e) {
-            e.printStackTrace();
-          }
-          return Optional.empty();
-    }
+    return Optional.empty();
+  }
 
-    @Override
-    public Optional<AccountType> remove(UUID id) {
-        Optional<AccountType> accountType = findBy(id);
-        if (accountType.isPresent()) {
-        try {
-            String sql = "DELETE FROM account_type WHERE account_type.id = ?";
-            int result = postQuery(sql, id);
-            if (result != -1 && result > 0) {
-            return accountType;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        }
-        return Optional.empty();
-    }
+  @Override
+  public List<AccountType> getAll() {
+    List<AccountType> list = new ArrayList<>();
 
-    private Optional<AccountType> fetch(String sql, Object value) throws SQLException {
-        resultSet = getQuery(sql, value.toString());
-            if (resultSet.next()) {
-              AccountType accountType = new AccountType();
-              
-              accountType.setId(UUID.fromString(resultSet.getObject(1).toString()));
-              accountType.setType(resultSet.getString(2));
-              accountType.setCreated(resultSet.getDate(3));
-              accountType.setUpdated(resultSet.getDate(4));
-      
-              return Optional.ofNullable(accountType);
-            }
-        return Optional.empty();
+    try {
+      String sql = "SELECT * FROM account_type;";
+      resultSet = getQuery(sql);
+      while (resultSet.next()) {
+        AccountType accountType = new AccountType();
+
+        accountType.setId(UUID.fromString(resultSet.getObject(1).toString()));
+        accountType.setType(resultSet.getString(2));
+        accountType.setCreated(resultSet.getDate(3));
+        accountType.setUpdated(resultSet.getDate(4));
+
+        list.add(accountType);
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
-    
+    closeConnection();
+    return list;
+  }
+
+  @Override
+  public Optional<AccountType> findBy(UUID id) {
+    try {
+      String sql = "SELECT * FROM account_type WHERE account_type.id = ?";
+      return this.fetch(sql, id);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<AccountType> remove(UUID id) {
+    Optional<AccountType> accountType = findBy(id);
+    if (accountType.isPresent()) {
+      try {
+        String sql = "DELETE FROM account_type WHERE account_type.id = ?";
+        int result = postQuery(sql, id);
+        if (result != -1 && result > 0) {
+          return accountType;
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    return Optional.empty();
+  }
+
+  private Optional<AccountType> fetch(String sql, Object value) throws SQLException {
+    resultSet = getQuery(sql, value.toString());
+    if (resultSet.next()) {
+      AccountType accountType = new AccountType();
+
+      accountType.setId(UUID.fromString(resultSet.getObject(1).toString()));
+      accountType.setType(resultSet.getString(2));
+      accountType.setCreated(resultSet.getDate(3));
+      accountType.setUpdated(resultSet.getDate(4));
+
+      return Optional.ofNullable(accountType);
+    }
+    return Optional.empty();
+  }
+
 }
--- src/main/java/com/javadb/service/AccountTypeService.java
+++ src/main/java/com/javadb/service/AccountTypeService.java
@@ -8,39 +8,39 @@ import com.javadb.bean.AccountType;
 import com.javadb.contracts.ServiceContract;
 import com.javadb.repository.AccountTypeRepository;
 
-public class AccountTypeService implements ServiceContract <AccountType, UUID> {
+public class AccountTypeService implements ServiceContract<AccountType, UUID> {
 
-    private AccountTypeRepository repo;
+  private AccountTypeRepository repo;
 
-    public AccountTypeService(AccountTypeRepository repo){
-        this.repo = repo;
-    }
+  public AccountTypeService(AccountTypeRepository repo) {
+    this.repo = repo;
+  }
 
-    @Override
-    public Optional<AccountType> create(AccountType accountType) {
-        return repo.add(accountType);
-    }
+  @Override
+  public Optional<AccountType> create(AccountType accountType) {
+    return repo.add(accountType);
+  }
 
-    @Override
-    public List<AccountType> getAll() {
-        return repo.getAll();
-    }
+  @Override
+  public List<AccountType> getAll() {
+    return repo.getAll();
+  }
 
-    @Override
-    public Optional<AccountType> findBy(UUID id) {
-        return repo.findBy(id);
-    }
+  @Override
+  public Optional<AccountType> findBy(UUID id) {
+    return repo.findBy(id);
+  }
 
-    @Override
-    public Optional<AccountType> update(UUID id, AccountType accountType) {
-        AccountType oldAccountType = repo.findBy(id).get();
-        oldAccountType.setType(accountType.getType());
-        return repo.add(oldAccountType);
-    }
+  @Override
+  public Optional<AccountType> update(UUID id, AccountType accountType) {
+    AccountType oldAccountType = repo.findBy(id).get();
+    oldAccountType.setType(accountType.getType());
+    return repo.add(oldAccountType);
+  }
 
-    @Override
-    public Optional<AccountType> delete(UUID id) {
-        return repo.remove(id);
-    }
-    
+  @Override
+  public Optional<AccountType> delete(UUID id) {
+    return repo.remove(id);
+  }
+
 }
